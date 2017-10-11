8a85321d1dc039d29d4ca4be39727a9335c0ac23
fananchong <fananchong@126.com>
2017-10-11 18:12:53 +0800

    up

--- chart_time_example.go
+++ chart_time_example.go
@@ -11,7 +11,7 @@ type TimeChartExample struct {
 }
 
 var (
-	playernum []int = []int{1, 5, 3, 6, 8, 0, 10, 11, 6, 10}
+	playernum []int = []int{1, 5, 3, 6, 8, 0, 10, 11, 6, 10, 11}
 )
 
 func (this *TimeChartExample) Update() {
@@ -22,17 +22,15 @@ func (this *TimeChartExample) Update() {
 	this.SubTitle = ""
 	this.YAxisText = "Num"
 	this.ValueSuffix = ""
-	this.TimeFormat = "second"
 
 	datas := make([]interface{}, 0)
 
 	json := simplejson.New()
 	json.Set("name", "player")
 	json.Set("data", playernum)
-	json.Set("pointInterval", 1)
-	json.Set("pointIntervalUnit", "second")
-	json.Set("pointStart", uint(time.Now().Unix()-int64(len(playernum))))
-	json.Set("pointEnd", uint(time.Now().Unix()))
+	json.Set("pointInterval", 1000)
+	json.Set("pointStart", 1000*uint(8*60*60+time.Now().Unix()-int64(len(playernum))))
+	json.Set("pointEnd", 1000*uint(8*60*60+time.Now().Unix()))
 	datas = append(datas, json)
 
 	json = simplejson.New()
